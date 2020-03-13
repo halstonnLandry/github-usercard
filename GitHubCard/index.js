2ae72c86882afdd1202f8e4e141629234f2e82ef
Halstonn Landry <hallrstein@gmail.com>
2020-03-12 21:41:10 -0500

    complete

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,18 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/halstonnLandry')
+.then(response=>{
+  console.log(response.data);
+  document.querySelector('.cards').append(userMake(response.data))
+  const followersArray = [response.data.followers];
+
+  })
+.catch(error=>{console.log(error);
+})
+
+
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -24,7 +36,7 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,7 +57,58 @@ const followersArray = [];
 </div>
 
 */
+//cb for creating tags
 
+//creating elements
+function userMake(user){
+  const cardMake=
+    document.createElement('div');
+  const imgMake=
+    document.createElement('img');
+    const infoMake=
+    document.createElement('div');
+  const nameMake=
+    document.createElement('h3');   
+  const usernameMake=
+    document.createElement('p');
+  const locationMake=
+    document.createElement('p');
+  const profileMake=
+    document.createElement('p');
+  const followersMake=
+    document.createElement('p');
+  const followingMake=
+    document.createElement('p');
+  const bioMake=
+    document.createElement('p');
+  const linkMake=
+    document.createElement('a');
+//appending elements
+cardMake.append(imgMake);
+cardMake.append(infoMake);
+infoMake.append(nameMake);
+infoMake.append(usernameMake);
+infoMake.append(locationMake);
+infoMake.append(profileMake);
+infoMake.append(followersMake);
+infoMake.append(followingMake);
+infoMake.append(bioMake);
+profileMake.append(linkMake);
+//applying classes
+  cardMake.classList.add('card');
+  infoMake.classList.add('card-info');
+  nameMake.classList.add('name');
+  usernameMake.classList.add('username');
+
+//applying content
+imgMake.src=user.avatar_url;
+nameMake.textContent=`${user.name}`;
+usernameMake.textContent=`${user.login}`;
+locationMake.textContent=`${user.location}`;
+linkMake.setAttribute('href',user.html_url);
+linkMake.textContent=user.html_url;
+  return cardMake;
+}
 /* List of LS Instructors Github username's: 
   tetondan
   dustinmyers
@@ -53,3 +116,25 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+const followersArray=[ 
+  'mtruj013',
+  'mrsimpson3000',
+  'easpaas',
+  '	MatthewHeideman',
+  'Riley-Robinson'
+];
+axios.get('https://api.github.com/users/halstonnLandry')
+.then(response=>{
+  followersArray.forEach(followers => {
+    axios.get(`https://api.github.com/users/${followers}`)
+  .then(response=>{
+    console.log(response.data);
+    document.querySelector('.cards').append(userMake(response.data))
+    })
+  .catch(error=>{console.log(error);
+  })
+  });
+  })
+.catch(error=>{console.log(error);
+})
